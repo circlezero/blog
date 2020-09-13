de6276ccefab57578cf575d2d822ada5a701b970
circlzero <jwy0565@gmail.com>
2020-09-13 22:21:09 +0900

    Fix: description about blog
    *  블로그 텍스트 수정
    * config 및 style 수정

--- config.js
+++ config.js
@@ -1,49 +1,51 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Circle Zero Web Blog",
+  subtitle:
+    "개발 블로그 / 삽질한 것을 어떻게 해결했는지, 쉽게 이해가 안가던것을 이해했을 때, 내 생각을 남기고 싶을 때 글을 남깁니다.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "DEV",
+      path: "/",
     },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
+    // {
+    //   label: "About me",
+    //   path: "/pages/about",
+    // },
+    // {
+    //   label: "Contact me",
+    //   path: "/pages/contacts",
+    // },
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "CircleZero",
+    photo: "/photo.jpg",
+    bio:
+      "개발 블로그 / 삽질한 것을 어떻게 해결했는지, 쉽게 이해가 안가던것을 이해했을 때, 내 생각을 남기고 싶을 때 글을 남깁니다.",
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "",
+      facebook: "",
+      telegram: "",
+      twitter: "https://twitter.com/dev_circlezero?s=09",
+      github: "https://github.com/circlezero",
+      rss: "",
+      vkontakte: "",
+      linkedin: "https://www.linkedin.com/in/wonyeong-cho-200113181/",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
